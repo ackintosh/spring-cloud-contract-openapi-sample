3753b4d05aeb4ec9ea2185d867883bd355931bd3
ackintosh <sora.akatsuki@gmail.com>
2020-01-30 18:14:24 +0900

    Fix the configurations to make `generateContractTests` task working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,34 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    // * You need to authenticate to GitHub Packages in order to install spring-cloud-contract-openapi.
+    // https://help.github.com/en/github/managing-packages-with-github-packages/configuring-gradle-for-use-with-github-packages#installing-a-package
+    // * For further details on declaring custom repositories see the document in gradle below:
+    // https://docs.gradle.org/current/userguide/declaring_repositories.html
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/ackintosh/spring-cloud-contract-openapi")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USER")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("KEY")
+            }
+        }
+    }
+
+    // * Please check out the latest version of spring-cloud-openapi.
+    // https://github.com/ackintosh/spring-cloud-contract-openapi/packages
+    dependencies {
+        classpath("com.github.ackintosh:spring-cloud-contract-openapi:0.0.1-SNAPSHOT")
+    }
+}
+
 plugins {
     id("org.springframework.boot") version "2.2.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
 
+    // * Spring Cloud Contract
     id("org.springframework.cloud.contract") version "2.2.1.RELEASE"
 }
 
@@ -15,18 +38,6 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
-
-    // You need to authenticate to GitHub Packages in order to install spring-cloud-contract-openapi.
-    // https://help.github.com/en/github/managing-packages-with-github-packages/configuring-gradle-for-use-with-github-packages#installing-a-package
-    // For further details on declaring custom repositories see the document in gradle below:
-    // https://docs.gradle.org/current/userguide/declaring_repositories.html
-    maven {
-        url = uri("https://maven.pkg.github.com/ackintosh/spring-cloud-contract-openapi")
-        credentials {
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("USER")
-            password = project.findProperty("gpr.key") as String? ?: System.getenv("KEY")
-        }
-    }
 }
 
 dependencies {
@@ -37,10 +48,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-
-    // Please check out the latest version of spring-cloud-openapi.
-    // https://github.com/ackintosh/spring-cloud-contract-openapi/packages
-    testImplementation("com.github.ackintosh:spring-cloud-contract-openapi:0.0.1-SNAPSHOT")
 }
 
 tasks.withType<Test> {
