9c2763cff3a1b06e8ca48bb59ce143be0bc0efd2
akihito-nakano <akihito.nakano@askul.com>
2020-01-30 16:28:04 +0900

    First commit

---
+++ src/main/kotlin/com/github/ackintosh/springcloudcontractopenapisample/SpringCloudContractOpenapiSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.github.ackintosh.springcloudcontractopenapisample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudContractOpenapiSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudContractOpenapiSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/ackintosh/springcloudcontractopenapisample/SpringCloudContractOpenapiSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.ackintosh.springcloudcontractopenapisample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCloudContractOpenapiSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
