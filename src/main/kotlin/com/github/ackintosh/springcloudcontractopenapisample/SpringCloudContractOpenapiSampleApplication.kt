bf3725fe33788375004b041a685d2383525eb427
ackintosh <sora.akatsuki@gmail.com>
2020-02-27 20:06:11 +0900

    Add an example endpoint

--- src/main/kotlin/com/github/ackintosh/springcloudcontractopenapisample/SpringCloudContractOpenapiSampleApplication.kt
+++ src/main/kotlin/com/github/ackintosh/springcloudcontractopenapisample/SpringCloudContractOpenapiSampleApplication.kt
@@ -2,10 +2,18 @@ package com.github.ackintosh.springcloudcontractopenapisample
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SpringCloudContractOpenapiSampleApplication
 
 fun main(args: Array<String>) {
     runApplication<SpringCloudContractOpenapiSampleApplication>(*args)
+}
+
+@RestController
+class Controller {
+    @GetMapping("/example")
+    fun example() = "Hello! Spring Cloud Contract OpenAPI!"
 }
